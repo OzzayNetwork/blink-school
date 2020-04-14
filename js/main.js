efcba970beea110c279bff5f629dab4067ff7a1c
ozzaynetworks <ozzaynetworks@gmail.com>
2020-04-14 19:47:08 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,149 @@
+$(window).on('load', function(){
+	
+	new WOW().init();
+	
+//	custom day time picker
+	$('#daily_date').on('change', function(){
+		var dated=$(this).val();
+		var dated=(moment(dated).format('LL'));
+		$('.the_day').text(dated);
+//		alert(moment(dated).format('LL'));
+	});
+	
+	
+//	$('.table-ranger').on('change', function(){
+//		var therange=$(this).val();
+//		alert(therange);
+//	});
+	
+	$('.table-ranger').on('change', function(){
+		
+	});
+//	increase wigdth when value changes
+	function Expand(obj){
+      if (!obj.savesize) obj.savesize=obj.size;
+      obj.size=Math.max(obj.savesize,obj.value.length);
+     }
+	
+	
+	
+//	date ranger functions
+	
+	$(".date-range")[0] && $(".date-range").flatpickr({
+      enableTime: !1,
+	  altInput: true,
+	   mode:"range",
+	   altFormat: "j-F, Y",
+	  dateFormat: "Y-m-d",
+	  maxDate: "today",
+	  minDate: "06-10-2019",
+		
+		
+      nextArrow: '<i class="zmdi zmdi-long-arrow-right" />',
+      prevArrow: '<i class="zmdi zmdi-long-arrow-left" />',
+		
+		//		write a function here when making changes in regards to date ranger
+		
+		onClose: function(selectedDates, dateStr, instance) {
+			var therange=$('#date_ranger').val();
+			
+			//selected date range
+			var dates=therange.split(' to ');
+			
+			//date in the format 2020-mm-dd
+			var date1=dates[0];
+			var date2=dates[1];
+			
+			// date in the format March 19, 2020
+			var date1read=moment(date1).format('ddd, MMMM Do YYYY');
+			var date2read=moment(date2).format('ddd, MMMM Do YYYY');
+			
+			//======= write custom functions bellow once a change has been made to the date range=======
+			
+			//function to update text output with new date range
+			$('.date-range-text').text(date1read+' To '+date2read);			
+			
+       
+    }
+    });
+//	full screen table
+
+	
+	 $("body").on("click", "[data-table-action]", function (a) {
+      a.preventDefault();
+      var b = $(this).data("table-action");
+      if ("print" === b && $(this).closest(".dataTables_wrapper").find(".buttons-print").trigger("click"), "fullscreen" === b) {
+        var c = $(this).parent().parent().parent().parent().parent().parent().parent()
+        c.hasClass("card--fullscreen") ? (c.removeClass("card--fullscreen"), $("body").removeClass("data-table-toggled")) : (c.addClass("card--fullscreen"), $("body").addClass("data-table-toggled"))
+      }
+    });
+//full screen controller
+	
+	
+//	datatable navigation styling
+	$('.nav-link').on('click', function(){
+		event.preventDefault();
+		$(this).addClass('active').parent().siblings().children('.nav-link').removeClass('active');
+	});
+
+	// $('td').eq(0).on('click', function(){
+	// 	$('.modal-body-header').children('span').html("nothing");
+
+	// 	var html_data=$('this').siblings().index(6).text();
+	// 	alert(html_data);
+	// });
+	
+	
+
+	$('.show-all').on('click', function(){
+			$('.table-all').removeClass('d-none').siblings().addClass('d-none');
+		});
+		
+		$('.show-compliant').on('click', function(){
+			$('.table-compliant').removeClass('d-none').siblings().addClass('d-none');
+		});
+		
+		$('.show-uncompliant').on('click', function(){
+			$('.table-uncompliant').removeClass('d-none').siblings().addClass('d-none');
+		});
+		
+		$('.show-penalty').on('click', function(){
+			$('.table-penalty').removeClass('d-none').siblings().addClass('d-none');
+		});
+	
+	
+	
+});
+
+//owl initializer
+$(document).ready(function(){
+
+	var owl = $('.owl-carousel');
+	owl.owlCarousel({
+		
+		items:4,
+		loop:true,
+		margin:10,
+		autoplay:true,
+		autoplayTimeout:15000,
+		autoplayHoverPause:true,
+		responsive:{
+			0:{
+				items:1
+			},
+			768:{
+				items:1
+			},
+			992:{
+				items:2
+			},
+			1200:{
+				items:3
+			},
+			1440:{
+				items:4
+			}
+		}
+	});
+
+  });
